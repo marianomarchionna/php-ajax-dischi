6e715dfcb6347876c9b5479ea7941b92dcca0ad4
Mariano <marianomarchionna8@gmail.com>
2021-10-12 16:02:49 +0200

    Aggiunta grafica card

--- index.php
+++ index.php
@@ -1,10 +1,47 @@
 <?php 
     include __DIR__ . '/partials/database.php';
-
-    foreach($db as $song){
-        echo 'Titolo: ' . $song['title'].'</br>';
-        echo 'Autore: ' . $song['author'].'</br>';
-        echo 'Genere: ' . $song['genre'].'</br>';
-        echo 'Anno: ' . $song['year'].'</br></br>';    
-    }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>PHP Dischi</title>
+</head>
+<body>
+    <header>
+        <div class="img-logo">
+            <img src="img/logo.png" alt="logo spotify">
+        </div>
+    </header>
+    <main>
+        <div class="songs-container">
+            <?php
+            foreach($db as $song){
+                echo "
+                <div class='single-song'>
+                    <div class='image'>
+                        <img src={$song['poster']} alt='poster {$song['title']}'>
+                        <h3 class='title'>
+                            {$song['title']}
+                        </h3>
+                        <div>
+                            <span class='author'>
+                                {$song['author']}
+                            </span>
+                            <span class='year'>
+                                {$song['year']}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                ";
+            }
+            ?>
+        </div>
+    </main>
+</body>
+</html>
